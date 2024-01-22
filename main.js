275092fe57660e45697aa5a5f442a0dce495855a
mac <mac@Macs-MacBook-Air.local>
2024-01-22 08:59:06 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+const form = document.getElementById("form");
+const emailForm = document.getElementById("email");
+const btnSubmit = document.querySelector(".btn");
+const usernameEl = document.querySelector("username");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   if (usernameEl === "") {
+//     btnSubmit.addEventListener("click", function () {
+//       usernameEl.value = `submit more details`;
+//       console.log(usernameEl);
+//     });
+//   }
+// });
