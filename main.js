7ecec6a18794754985026dae4ed56df5ea713c63
mac <mac@Macs-MacBook-Air.local>
2024-01-24 09:56:12 +0100

    icon corrected

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const emailForm = document.getElementById("email");
 const btnSubmit = document.querySelector(".btn");
 const usernameEl = document.querySelector("username");
 
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   if (usernameEl === "") {
-//     btnSubmit.addEventListener("click", function () {
-//       usernameEl.value = `submit more details`;
-//       console.log(usernameEl);
-//     });
-//   }
-// });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (usernameEl === "") {
+    btnSubmit.addEventListener("click", function () {
+      usernameEl.value = `submit more details`;
+      console.log(usernameEl);
+    });
+  }
+});
